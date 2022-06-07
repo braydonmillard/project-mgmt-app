14fc994a364df3e45ffe3f1ecaefb003afaaf01a
braydonmillard <46946203+braydonmillard@users.noreply.github.com>
2022-06-07 17:02:52 -0400

    Implemented MongoDB connection

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,9 @@
 const { projects, clients } = require('../sampleData.js')
 
+// Mongoose models
+const Project = require('../models/Project');
+const Client = require('../models/Client');
+
 const { GraphQLObjectType,
     GraphQLID,
     GraphQLString,
@@ -14,8 +18,13 @@ const ProjectType = new GraphQLObjectType({
         id: { type: GraphQLID },
         name: { type: GraphQLString },
         description: { type: GraphQLString},
-        status: { type: GraphQLString}
-    })
+        status: { type: GraphQLString},
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.findById(parent.clientId);            }
+        }
+    }),
 });
 
 //Client Type
@@ -35,28 +44,28 @@ const RootQuery = new GraphQLObjectType({
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args){
-                return projects;
-            }
+                return Project.find();
+            },
         },
         project: {
             type: ProjectType,
             args: { id: {type: GraphQLID } },
             resolve(parent, args) {
-                return projects.find(project => project.id === args.id);
+                return Project.findById(args.id);
             }
         },
 
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args){
-                return clients;
+                return Client.find();
             }
         },
         client: {
             type: ClientType,
             args: { id: {type: GraphQLID } },
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id);
+                return Client.findById(args.id);
             }
         }
     }
